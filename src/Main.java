69f70c6ec6699501432e92a58c57e78454c4de17
latsa <latsapnev01@mail.ru>
2023-12-23 20:12:44 +0300

    Task №1

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,20 @@
 public class Main {
-    public static void main(String[] args) {System.out.println("Hello world");
+    public static void main(String[] args) {
+
+        //Task 1
+
+        int deposit = 15_000;
+        int total = 2_459_000;
+        int sum = 0;
+        int month = 1;
+        while (true){
+            sum = sum + sum/100;
+            sum = sum + deposit;
+            System.out.println("Месяц "+month+", сумма накоплений равна "+sum+" рублей");
+            month++;
+            if (sum >= total){
+                break;
+            }
+        }
     }
 }
