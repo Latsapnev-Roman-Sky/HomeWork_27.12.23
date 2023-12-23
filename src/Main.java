39e4032b8cf0aa9b3abb5514ca391cdc1d22e130
latsa <latsapnev01@mail.ru>
2023-12-23 20:24:02 +0300

    Task №2

--- src/Main.java
+++ src/Main.java
@@ -16,5 +16,19 @@ public class Main {
                 break;
             }
         }
+
+        //Task 2
+
+        int i = 1;
+        while (i <= 10){
+            System.out.print(i+" ");
+            i++;
+        }
+        System.out.println();
+
+        for (i = 10; i > 0; i--){
+            System.out.print(i+" ");
+        }
+        System.out.println();
     }
 }
